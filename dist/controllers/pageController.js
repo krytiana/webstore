49d48a38ad3d069aa1483e1bac99b114db4d202b
ignatus2767 <ignatusdonkoh9@gmail.com>
2026-03-17 18:12:41 +0000

    Remove env, dist, node_modules and add gitignore

--- dist/controllers/pageController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.homePage = void 0;
-const homePage = (req, res) => {
-    res.render("home", { title: "Ready-Made Websites" });
-};
-exports.homePage = homePage;
